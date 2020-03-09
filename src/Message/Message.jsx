426bbd4022891b216c315b460894bd5ea2166b83
RSheremet <roma.sheremet.91@mail.ru>
2020-03-09 23:13:38 +0300

    At last

---
+++ src/Message/Message.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import c from './Message.module.css';
+import Photo from "./Photo";
+
+const Message = (props) => {
+    return (
+        <div className={c.wrapper}>
+            <div className={c.mesWrapper}>
+                <Photo />
+                <div className={c.message}>
+                    <div className={c.date}>
+                        08.03.2020
+                    </div>
+                    <div className={c.theMessage}>
+                        Aliena vitia in oculus habemus a tergo nostra sunt. Aliena vitia in oculus habemus a tergo nostra sunt. Aliena vitia in oculus habemus a tergo nostra sunt. Aliena vitia in oculus habemus a tergo nostra sunt. Aliena vitia in oculus habemus a tergo nostra sunt. Aliena vitia in oculus habemus a tergo nostra sunt.
+                    </div>
+                    <div className={c.time}>
+                        22:09
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Message;
---
+++ src/Message/Photo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import c from './Message.module.css';
+
+const Photo = (props) => {
+    return (
+        <div className={c.photo}>
+        </div>
+    )
+}
+
+export default Photo;
