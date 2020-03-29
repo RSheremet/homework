13ebbcdc2c4d588d79134f59a5b45be705285a4e
RSheremet <roma.sheremet.91@mail.ru>
2020-03-29 19:39:14 +0400

    Updates

---
+++ precache-manifest.6c02278dbb894502b7500e28e67cfa9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52d32d62de38daef32bb7308c4bc0dcc",
+    "url": "/homework/index.html"
+  },
+  {
+    "revision": "d53eabfc67b1661e5353",
+    "url": "/homework/static/css/main.5db05a3a.chunk.css"
+  },
+  {
+    "revision": "e44f47dd16c510e55a76",
+    "url": "/homework/static/js/2.57cae78f.chunk.js"
+  },
+  {
+    "revision": "d53eabfc67b1661e5353",
+    "url": "/homework/static/js/main.c7acb48a.chunk.js"
+  },
+  {
+    "revision": "8bd44581aea2e3cc87fb",
+    "url": "/homework/static/js/runtime~main.1bb8d28a.js"
+  }
+]);
